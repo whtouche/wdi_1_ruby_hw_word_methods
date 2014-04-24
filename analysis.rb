58a7f32bf0870f255385e060ecaa02e388ea2753
Alex Grant <grantovich@users.noreply.github.com>
2014-04-24 09:22:47 -0400

    Create method to split text into normalized words

--- analysis.rb
+++ analysis.rb
@@ -1,2 +1,10 @@
+require 'pry'
+
 # This will give you the contents of the `sample.txt` file as one big string
 sample_text = File.read('sample.txt')
+
+def normalized_words(text)
+  text.downcase.gsub(/[^a-z]/, ' ').split
+end
+
+binding.pry
